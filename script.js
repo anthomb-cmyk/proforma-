c140a11b70c4fe311df30a5e1a613c07446a7f9b
anthomb-cmyk <anthomb@hotmail.com>
2026-03-18 19:58:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const SUPABASE_URL = "https://nuuzkvgyolxbawvqyugu.supabase.co";
 const SUPABASE_KEY = "sb_publishable_103-rw3MwM7k2xUeMMUodg_fRr9vUD4";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const chatState = {
   currentMode: "listing",
